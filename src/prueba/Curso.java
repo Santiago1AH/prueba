e8d033f79bb649e64e598e4ec323d04e1e22295b
Santiago Arroyave <arroyaves956@gmail.com>
2021-09-09 22:11:27 -0500

    Se agrego la descripcion  de la clase del Curso

---
+++ src/prueba/Curso.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package prueba;
+
+/**
+ *  Esta es la clase entidad que representa a un curso 
+ * @author Santiago
+ */
+public class Curso {
+
+}
